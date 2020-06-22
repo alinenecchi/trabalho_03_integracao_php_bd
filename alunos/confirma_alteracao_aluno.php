ab47dd97188fbddc4cac7466a30d940d3e3ee2ea
alinecruz87 <59289746+alinecruz87@users.noreply.github.com>
2020-06-22 20:11:11 -0300

    funcionando

--- alunos/confirma_alteracao_aluno.php
+++ alunos/confirma_alteracao_aluno.php
@@ -18,24 +18,22 @@
 <?php
 	$Matricula=$_GET['cod_alter'];
 	$nome_alter=$_GET['nome_alter'];
-	$endereco_alter=$GET['endereco_alter']
-	$cidade_alter=$GET['cidade_alter']
-	$estado_alter=$GET['estado_alter']
+	$endereco_alter=$_GET['endereco_alter'];
+	$cidade_alter=$_GET['cidade_alter'];
 	$curso_alter=$_GET['curso_alter'];
 	//falta verificar se campos estão preenchidos
-	require("conecta.inc.php");
+	require("../conecta.inc.php");
 	$ok = conecta_bd() or die ("Não é possível conectar-se ao servidor.");
-	mysqli_query($ok, "update alunos set nomea='$nome_alter', endereco='$endereco_alter', cidade='$cidade_alter', estado='$estado_alter' where cod_aluno='$Matricula'") or die ("Não é possível deletar dados do Aluno!!");
-	print("Alteração realizada:<p>");
-	print(	"Matrícula: $Matricula 
-			Nome: <b>$nome_alter</b>
-			Endereço:  $endereco_alter 
-			Cidade: $cidade_alter 
-			Estado: $estado_alter
-			Curso: $curso_alter <p>");
+	mysqli_query($ok, "update alunos set nome_aluno='$nome_alter', endereco='$endereco_alter', cidade='$cidade_alter', curso ='$curso_alter' where matricula='$Matricula'") or die ("Não é possível deletar dados do Aluno!!");
+	print("<br>Alteração realizada:<p>");
+	print(	"<br>Matrícula: $Matricula 
+			<br>Nome: <b>$nome_alter</b>
+			<br>Endereço:  $endereco_alter 
+			<br>Cidade: $cidade_alter 
+			<br>Curso: $curso_alter <p>");
 
 	print("Dados alterados com sucesso!");
 ?>
-<p><a href="./index.php">Página Inicial</a>
+<p><a href="../index.php">Página Inicial</a>
   </body>
 </html>
