94e2353022a6a8cc73bd63083263c12a99b02e55
alinecruz87 <59289746+alinecruz87@users.noreply.github.com>
2020-06-23 15:09:36 -0300

    funcionando ok

--- alunos/altera_alunos.php
+++ alunos/altera_alunos.php
@@ -2,14 +2,14 @@
 	$codigo=$_GET['cod'];
 	require("../conecta.inc.php");
 	$ok = conecta_bd() or die ("Não é possível conectar-se ao servidor.");
-	$resultado1=mysqli_query($ok, "Select * from alunos, cidades, cursos where matricula= '$codigo'");
+	$resultado1=mysqli_query($ok, "Select * from alunos, cidades, cursos where cidades.cod_cidade=alunos.cidade and cursos.codigo=alunos.curso and matricula= '$codigo'");
 	$linha=mysqli_fetch_array($resultado1);
 	$Matricula  =$linha["matricula"];
 	$Nome       =$linha["nome_aluno"];
 	$Endereco   =$linha["endereco"];
-   $Cidade     =$linha["nome_cid"];
-   $Cod_cidade =$linha["cod_cidade"];
-   $Curso      =$linha["nome_curso"];
+	$Cidade     =$linha["nome_cid"];
+	$Cod_cidade =$linha["cod_cidade"];
+	$Curso      =$linha["nome_curso"];
 	$Cod_curso  =$linha["codigo"];
 	print("<h1>Alterando os dados do aluno: $Nome</h1><p>");
 ?>
@@ -23,9 +23,9 @@
 $resultado2=mysqli_query($ok, "Select * from cidades where cod_cidade <> '$Cod_cidade' order by nome_cid") or die ("Não é possível consultar as cidades"); 
 while ($linha=mysqli_fetch_array($resultado2))
 {
-   $Cod_cidade=$linha["cod_cidade"];
-   $Cidade=$linha["nome_cid"];
-   print("<option value='$Cod_cidade'>$Cidade</option>");
+	$Cod_cidade=$linha["cod_cidade"];
+	$Cidade=$linha["nome_cid"];
+	print("<option value='$Cod_cidade'>$Cidade</option>");
 };
 ?>
 </select>
@@ -35,9 +35,9 @@ while ($linha=mysqli_fetch_array($resultado2))
 $resultado3=mysqli_query($ok, "Select * from cursos where codigo <> '$Cod_curso' ") or die ("Não é possível consultar os cursos"); 
 while ($linha=mysqli_fetch_array($resultado3))
 {
-   $Codigo_curso=$linha["codigo"];
-   $Nome_curso=$linha["nome_curso"];
-   print("<option value='$Codigo_curso'>$Nome_curso</option>");
+	$Codigo_curso=$linha["codigo"];
+	$Nome_curso=$linha["nome_curso"];
+	print("<option value='$Codigo_curso'>$Nome_curso</option>");
 }
 ?>
 </select>
