7aa90bdd0b2bf7c1332fdaa3f70518d434b63069
brossoni <56612299+brossoni@users.noreply.github.com>
2020-06-27 20:10:27 -0300

    css aluno_a_deletar.php

--- alunos/aluno_a_deletar.php
+++ alunos/aluno_a_deletar.php
@@ -26,35 +26,47 @@
         </div>
       </a>
     </section>
+<div id="infos">
+  <?php
+      $codigo = $_GET['cod'];
+      require("../conecta.inc.php");
+      $ok = conecta_bd() or die ("Não é possível conectar-se ao servidor.");
+      $resultado1=mysqli_query($ok, "Select * from alunos, cidades, cursos where cidades.cod_cidade=alunos.cidade and cursos.codigo=alunos.curso and matricula= '$codigo'")or die ("Não é possível retornar os dados");
+      $linha=mysqli_fetch_array($resultado1);
+      $Matricula  =$linha["matricula"];
+      $Nome       =$linha["nome_aluno"];
+      $Endereço   =$linha["endereco"];
+      $Cidade     =$linha["nome_cid"];
+      $Curso      =$linha["nome_curso"];
+      
+      print("<p><b>Deletando o aluno(a)</b></p>");
+      print("Matricula: $Matricula<br>");
+      print("Nome: <b>$Nome</b><br>");
+      print("Endereço: $Endereço<br>");
+      print("Cidade: $Cidade<br>");
+      print("Curso: $Curso");
 
-<?php
-    $codigo = $_GET['cod'];
-    require("../conecta.inc.php");
-    $ok = conecta_bd() or die ("Não é possível conectar-se ao servidor.");
-    $resultado1=mysqli_query($ok, "Select * from alunos, cidades, cursos where cidades.cod_cidade=alunos.cidade and cursos.codigo=alunos.curso and matricula= '$codigo'")or die ("Não é possível retornar os dados");
-    $linha=mysqli_fetch_array($resultado1);
-    $Matricula  =$linha["matricula"];
-    $Nome       =$linha["nome_aluno"];
-    $Endereço   =$linha["endereco"];
-    $Cidade     =$linha["nome_cid"];
-    $Curso      =$linha["nome_curso"];
-    
-    print("<h3>Deletando o aluno(a)</h3><p>");
-    print("Matricula: $Matricula<br>");
-    print("Nome: <b>$Nome</b><br>");
-    print("Endereço: $Endereço<br>");
-    print("Cidade: $Cidade<br>");
-    print("Curso: $Curso");
-
-?>
-  <div>
-    <form action="./aluno_deletado.php" method="POST">
-      <input type="hidden" name="cod_del" value="<?php print($Matricula)?>">
-      <input type="hidden" name="nome" value="<?php print($Nome)?>">
-      <input name ="submit" type="submit" value="Confirmar Deletar"/>
-    </form>
-    <button><a href="./lista_alunos.php">Cancelar e voltar</a></button>
+  ?>
+    <div>
+      <form action="./aluno_deletado.php" method="POST">
+        <input type="hidden" name="cod_del" value="<?php print($Matricula)?>">
+        <input type="hidden" name="nome" value="<?php print($Nome)?>">
+        <input id="confirmaDeletar" name ="submit" type="submit" value="Confirmar Deletar"/>
+      </form>
+    </div>
   </div>
+
+  <section id="cancelarNav";>
+			<a href="lista_alunos.php">
+				<div id="CancelarButton">
+					<img src="../images/cancel.png" id="cancelarIcon">
+				</div>
+
+				<div id="cancelarText">
+					Cancelar
+				</div>
+			</a>
+		</section>
 
 </body>
 </html>
